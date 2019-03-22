d5316294cdc1747a738b7d1d03727b631d11288b
eclay jebet <eclay.jebet@welltoldstory.com>
2019-03-22 16:33:24 +0300

    sidebar

--- src/Component/Navigation/MenuFolder/Menu.js
+++ src/Component/Navigation/MenuFolder/Menu.js
@@ -20,9 +20,38 @@ class Menu extends Component {
           onMouseDown={this.props.handleMouseDown}
           className={visibility}
         >
-          <Link to="/home">Dashboard</Link>
-          <Link to="/daily">Daily</Link>
-          <Link to="#">Home</Link>
+          <div className="links">
+            <div id="dismiss">
+              <i class="fa fa-times" />
+            </div>
+            <h6>
+              <Link to="/dashboard/" style={{ color: "white" }}>
+                <i class="fas fa-tachometer-alt" />
+                Dashboard
+              </Link>
+            </h6>
+            <br />
+            <h6>
+              <Link to="/daily" style={{ color: "white" }}>
+                <i class="far fa-comment-alt" />
+                Daily
+              </Link>
+            </h6>
+            <br />{" "}
+            <h6>
+              <Link to="/airtime" style={{ color: "white" }}>
+                <i class="fas fa-share-square" />
+                Airtime
+              </Link>
+            </h6>
+            <br />
+            <h6>
+              <Link to="/settings" style={{ color: "white" }}>
+                <i class="fas fa-cog" />
+                Settings
+              </Link>
+            </h6>
+          </div>
         </div>
       </Router>
     );

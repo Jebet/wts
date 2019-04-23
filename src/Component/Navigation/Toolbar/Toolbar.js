f49f1a7ef423c3b04f2f91f04f87b5ca16fb7ec9
eclay jebet <eclay.jebet@welltoldstory.com>
2019-04-23 12:42:34 +0300

    modified logout route

--- src/Component/Navigation/Toolbar/Toolbar.js
+++ src/Component/Navigation/Toolbar/Toolbar.js
@@ -1,30 +1,10 @@
 import React from "react";
 
 import "./Toolbar.css";
-//import Logo from '../../Logo/Logo';
 import NavigationItems from "../NavigationItems/Navigation";
 import DrawerToggle from "../SideDrawer/ToggleDrawer/ToggleDrawer";
 import logo from "../../../Assets/logo/logo.png";
 import Menu from "../MenuFolder/Menu";
-// import SidebarExampleSidebar from "../SideBar/sideBar";
-
-// const toolbar = props => (
-//   <header className="Toolbar">
-//     <Menu />
-//     <DrawerToggle clicked={props.drawerToggleClicked} />
-//     <div className="toolbar-logo">
-//       <a href="/home">
-//         <p>Shujaaz</p>
-//       </a>
-//     </div>
-
-//     <nav className="DesktopOnly">
-//       <NavigationItems />
-//     </nav>
-//   </header>
-// );
-
-// export default toolbar;
 
 class toolbar extends React.Component {
   constructor(props) {
@@ -62,7 +42,7 @@ class toolbar extends React.Component {
               <a href="/daily" onClick={this.burgerToggle}>
                 Dailies
               </a>
-              <a href="/logout" onClick={this.burgerToggle}>
+              <a href="/login" onClick={this.burgerToggle}>
                 Logout
               </a>
             </div>
